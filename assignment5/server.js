22f22c1c0f058efea317907a8c9b9a48d868822f
gayanebabayan <g.babayan@yahoo.com>
2019-05-02 02:09:15 -0400

    Update server.js

--- assignment5/server.js
+++ assignment5/server.js
@@ -1,15 +1,3 @@
-/*********************************************************************************
-* WEB322 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Name: GAYANE BABAYAN Student ID: 107-061-160 Date: 7-SEPT-2018
-*
-* Online (Heroku) URL: _______________________________________________________
-*
-********************************************************************************/ 
-
 
 var express = require ("express");
 var app = express();
